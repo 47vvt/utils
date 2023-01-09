2b44947de2a305f0077d584c0436cc44cf2ac302
47vvt <116542696+47vvt@users.noreply.github.com>
2023-01-09 23:14:11 +1100

    Add files via upload

---
+++ ax_jupyter_install.ps1
@@ -0,0 +1,12 @@
+Invoke-WebRequest -Uri https://bootstrap.pypa.io/get-pip.py -OutFile .\get-pip.py
+python get-pip.py
+pip install nodejs
+pip install npm
+pip install algorithmx
+exit()
+
+jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build
+jupyter lab build
+
+code --install-extension ms-python.python
+code --install-extension ms-toolsai.jupyter
