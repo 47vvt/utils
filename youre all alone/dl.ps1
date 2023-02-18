d0528b434824512dc40dd8e85b6d4b670f269a8b
47vvt <116542696+47vvt@users.noreply.github.com>
2023-02-18 22:58:31 +1100

    x.gd/ceiling

---
+++ youre all alone/dl.ps1
@@ -0,0 +1,13 @@
+
+set-location $env:userprofile\music
+mkdir "youre all alone"
+set-location "$env:userprofile\music\youre all alone"
+invoke-webrequest https://github.com/47vvt/utils/raw/main/youre%20all%20alone/22.mp3 -o 22.mp3
+invoke-webrequest https://github.com/47vvt/utils/raw/main/youre%20all%20alone/k.mp3 -o k.mp3
+invoke-webrequest https://github.com/47vvt/utils/raw/main/youre%20all%20alone/leave%20you%20behind.mp3 -o "leave you behind.mp3"
+invoke-webrequest https://github.com/47vvt/utils/raw/main/youre%20all%20alone/may%20as%20well.mp3 -o "may as well.mp3"
+invoke-webrequest https://github.com/47vvt/utils/raw/main/youre%20all%20alone/old%20enough%20to%20understand.mp3 -o "old enough to understand.mp3"
+invoke-webrequest https://github.com/47vvt/utils/raw/main/youre%20all%20alone/sanctuary.mp3 -o "sanctuary.mp3"
+invoke-webrequest https://github.com/47vvt/utils/raw/main/youre%20all%20alone/tired.mp3 -o "tired.mp3"
+invoke-webrequest https://github.com/47vvt/utils/raw/main/youre%20all%20alone/ykwim.mp3 -o "ykwim.mp3"
+invoke-webrequest https://github.com/47vvt/utils/raw/main/youre%20all%20alone/youre%20all%20alone.mp3 -o "youre all alone.mp3"
