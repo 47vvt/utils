2ee7f8c11da7c51906693daf7e0fdcdb7b8a0b6f
47vvt <116542696+47vvt@users.noreply.github.com>
2023-03-13 11:54:59 +1100

    Update 12ALG.ps1

--- 12ALG.ps1
+++ 12ALG.ps1
@@ -1,14 +1,11 @@
 
-# installs a bunch of stuff to make python work for VCE algo HESS
+# script to make python graph simulations for VCE algo HESS
 # irm x.gd/12alga | iex
 
 Invoke-WebRequest -Uri https://bootstrap.pypa.io/get-pip.py -OutFile .\get-pip.py
 python get-pip.py
-pip install nodejs
-pip install npm
 pip install algorithmx
 pip install networkx
-pip install pandas
 pip install jupyter
 code --install-extension ms-python.python
 code --install-extension ms-toolsai.jupyter
